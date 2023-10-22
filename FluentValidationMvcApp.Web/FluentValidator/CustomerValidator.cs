c3b3f9f0bcaba843c6ffd77da151b82e6a7a5fa9
huseyincan <dincerhuseyincan@gmail.com>
2023-10-22 15:57:34 +0300

    Project Initial

---
+++ FluentValidationMvcApp.Web/FluentValidator/CustomerValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FluentValidationMvcApp.Web.Models;
+
+namespace FluentValidationMvcApp.Web.FluentValidator
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim alanı boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez").EmailAddress().WithMessage("Bu alan email tipinde olmalıdır");
+            RuleFor(x => x.Age).NotEmpty().WithMessage("Yaş alanı boş geçilemez").InclusiveBetween(18, 60).WithMessage("Yaş alanı 18 - 60 yaş aralığında olmalıdır.");
+
+        }
+    }
+}
